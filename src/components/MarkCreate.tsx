db18b9026ca95360cc296f1eff23f31211514621
Leonardo Henao <leonardofhenao.085@gmail.com>
2024-05-19 14:23:53 -0500

    Update mark autor

--- src/components/MarkCreate.tsx
+++ src/components/MarkCreate.tsx
@@ -1,6 +1,6 @@
 const MarkCreate = () => {
   return (
-    <small className="text-end block absolute bottom-2 right-2">
+    <small className="text-end block lg:absolute  bottom-2 right-2">
       Create with ❤️ by{' '}
       <a href="http://leonardohenao.com" target="_blank" rel="noopener noreferrer">
         @lhenaoll
